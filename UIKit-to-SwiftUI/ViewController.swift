06cfb6e03ec12a65e591737b07c235b3fd9ec0e8
Eugene <e@egeek.me>
2021-11-15 20:48:15 +0200

    Initial commit

---
+++ UIKit-to-SwiftUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit-to-SwiftUI
+//
+//  Created by u on 15.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
