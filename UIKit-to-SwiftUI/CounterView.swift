80da74ea3ac6ce722f6d9f5e9c78afbfd1a229bc
Eugene <e@egeek.me>
2021-11-19 20:17:36 +0200

    Support missing counters

--- UIKit-to-SwiftUI/CounterView.swift
+++ UIKit-to-SwiftUI/CounterView.swift
@@ -8,16 +8,17 @@
 import SwiftUI
 
 struct CounterView: View {
-    var counter = 0
+    var counter: Int? = 0
 
     var body: some View {
-        Text("Counter: \(counter)")
+        Text(counter.map { "Counter: \($0)" }
+            ?? "No counter")
     }
 }
 
 struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach([0, 42], id: \.self) { counter in
+        ForEach([0, 42, nil], id: \.self) { counter in
             CounterView(counter: counter)
                 .previewLayout(.fixed(width: 200, height: 100))
         }
