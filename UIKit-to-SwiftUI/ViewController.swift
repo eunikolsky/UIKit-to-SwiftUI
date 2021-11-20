1b6f1e8ae0679d84df10e15ec3cc1d3c5b4fc8d1
Eugene <e@egeek.me>
2021-11-20 09:06:30 +0200

    Capture `CounterHostingController` in `ViewController`

--- UIKit-to-SwiftUI/ViewController.swift
+++ UIKit-to-SwiftUI/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var counterController: CounterHostingController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "counterSegue": counterController = segue.destination as? CounterHostingController;
+        default: break;
+        }
+    }
 
     @IBAction func didPressIncreaseButton(_ sender: UIButton) {
     }
